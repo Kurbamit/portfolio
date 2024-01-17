d1b34a42262190cb06f9b20839aad685775b7fa0
Dominykas Černovas <cernovas50@gmail.com>
2024-01-17 18:55:49 +0200

    Fixed pictures

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
          title: "Project 1",
-         imgUrl: "/assets/terminal-website.jpg",
+         imgUrl: "./assets/terminal-website.jpg",
          stack: ["HTML", "CSS", "JavaScript"],
          link: "https://www.linkedin.com/in/dominykas-cernovas/"
     },
     {
         title: "Developer Portfolio",
-        imgUrl: "/assets/terminal-website.jpg",
+        imgUrl: "./assets/terminal-website.jpg",
         stack: ["React", "Tailwind", "Vite "],
         link: "https://www.linkedin.com/in/dominykas-cernovas/"
    },
    {
     title: "Project 3 ",
-    imgUrl: "/assets/terminal-website.jpg",
+    imgUrl: "./assets/terminal-website.jpg",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://www.linkedin.com/in/dominykas-cernovas/"
 }
