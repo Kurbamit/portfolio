dbe31083beede3bde39301c88dec6f299d08125c
Dominykas Černovas <cernovas50@gmail.com>
2024-01-20 14:38:49 +0200

    Added more things to the timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -9,6 +9,18 @@ export default [
         year: "2022",
         title: "Bachelor's degree in Software Engineering",
         duration: "2022-Present",
-        details: "I am currently a second year student at the Vilnius University of Software Engineering."
+        details: "I am currently a second year student at the Vilnius University of Software Engineering. "
+    },
+    {
+        year: "2022",
+        title: "Graduation from gymnasium",
+        duration: "2018-2022",
+        details: "I graduated from the gymnasium in 2022. I was a member of the school's volleyball team and I was also a member of the school's student council. The big journey to the digital world began here."
+    },
+    {
+        year: "2018",
+        title: "First line of code",
+        duration: "the beginning",
+        details: "I wrote my first line of code in 2018. It was a simple 'Hello World' program in C++. I was so excited that I decided to learn more about programming. I started learning C++ more seriously and I was able to create a simple calculator."
     }
 ]
