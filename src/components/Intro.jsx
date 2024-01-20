61a386d646196408e5b6f89e8e263371435ce901
Dominykas Černovas <cernovas50@gmail.com>
2024-01-20 14:33:42 +0200

    Added timeline and bio

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,20 @@ function Intro() {
       </h1>
       <p className="text-base md:text-xl mb-3 font-medium">Software engineer</p>
       <p className="text-sm max-w-xl mb-6 font-bold">
-        This is going to be my bio
+        My name is Dominykas and I am a software engineer based in Vilnius,
+        Lithuania. I enjoy creating things that live on the internet, whether
+        that be websites, applications, or anything in between. I am always
+        learning new things, currently those things are .NET and React. You can
+        find me on{" "}
+        <a
+          href="https://github.com/Kurbamit"
+          target="_blank"
+          className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
+          rel="noreferrer noopener"
+        >
+          Kurbamit Github
+        </a>
+        .
       </p>
     </div>
   );
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,26 +1,14 @@
 export default [
     {
-        year: "Today",
+        year: "2023",
+        title: "Sales director's assistant",
+        duration: "2023-Present",
+        details: "I am currently working at a company called 'UADBB Rizikos valdymo specialistai' as a sales director's assistant. I am responsible for preparation of various documents and insurance offers."
+    },
+    {
+        year: "2022",
         title: "Bachelor's degree in Software Engineering",
         duration: "2022-Present",
         details: "I am currently a second year student at the Vilnius University of Software Engineering."
-    },
-    {
-        year: "2021",
-        title: "Freelance",
-        duration: "2021-2022",
-        details: "I worked as a freelance web developer for a year. I have created a few websites for clients."
-    },
-    {
-        year: "2023",
-        title: "Web Developer",
-        duration: "2023-Present",
-        details: "I am currently working as a web developer for a company called 'Web Solutions'."
-    },
-    {
-        year: "2023",
-        title: "Web Developer",
-        duration: "2023-Present",
-        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     }
 ]
