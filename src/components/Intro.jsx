ed397b446a5dbf6d7e9c5a94ad3463ab809750b0
Dominykas Černovas <cernovas50@gmail.com>
2025-06-11 21:37:24 +0300

    Adde LinkedIn link

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,11 +8,19 @@ function Intro() {
       </h1>
       <p className="text-base md:text-xl mb-3 font-medium">Software engineer</p>
       <p className="text-sm max-w-xl mb-6 font-bold">
-        My name is Dominykas and I am a software engineer based in Vilnius,
-        Lithuania. I enjoy creating things that live on the internet, whether
-        that be websites, applications, or anything in between. I am always
-        learning new things, currently those things are .NET and React. You can
-        find me on{" "}
+        My name is{" "}
+        <a
+          href="https://www.linkedin.com/in/dominykas-cernovas/"
+          target="_blank"
+          className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
+          rel="noreferrer noopener"
+        >
+          Dominykas
+        </a>{" "}
+        and I am a software engineer based in Vilnius, Lithuania. I enjoy
+        creating things that live on the internet, whether that be websites,
+        applications, or anything in between. I am always learning new things,
+        currently those things are .NET and React. You can find me on{" "}
         <a
           href="https://github.com/Kurbamit"
           target="_blank"
